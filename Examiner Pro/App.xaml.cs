4d88ddf57af7376c55ca918ebc6a974d2d1361fe
Tariq Mehmood <talknchat@gmail.com>
2014-12-15 14:52:00 +0300

    Added some theming to the application. Login and Main window.

--- Examiner Pro/App.xaml.cs
+++ Examiner Pro/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Examiner_Pro.Examiner.GUI;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,26 @@ namespace Examiner_Pro
     /// </summary>
     public partial class App : Application
     {
+        public  void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            //Disable shutdown when the dialog closes
+            Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            var dialog = new LoginWindow();
+
+            if (dialog.ShowDialog() == true)
+            {
+                var mainWindow = new MainWindow();
+                //Re-enable normal shutdown mode.
+                Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+                Current.MainWindow = mainWindow;
+                mainWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Unable to load data.", "Error", MessageBoxButton.OK);
+                Current.Shutdown(-1);
+            }
+        }
     }
 }
---
+++ Examiner Pro/Examiner.GUI/LoginWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Examiner_Pro.Examiner.GUI
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxFirstName.Text = "";
+            passwordBox1.Clear();
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            //We need to perform the login and set the login to true.
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
