52371947d45a868401d5afea749215aa6698ddfe
Tariq Mehmood <talknchat@gmail.com>
2014-12-22 17:50:22 +0300

    Added couple of data structures.

---
+++ Examiner Pro/Examiner.GUI/Exams/AddWizard/Question.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examiner_Pro.Examiner.GUI.Exams.AddWizard
+{
+    class Question
+    {
+        List<QuestionOption> _questions = new List<QuestionOption>();
+        QuestionData _data = new QuestionData();
+        public List<QuestionOption> Questions
+        {
+            get
+            {
+                return _questions;
+            }
+            set
+            {
+                _questions = value;
+            }
+        }
+
+        public QuestionData Data
+        {
+            get
+            {
+                return _data;
+            }
+            set
+            {
+                _data = value;
+            }
+        }
+
+    }
+}
---
+++ Examiner Pro/Examiner.GUI/Exams/AddWizard/QuestionOption.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examiner_Pro.Examiner.GUI.Exams.AddWizard
+{
+    public class QuestionOption
+    {
+        Boolean _correct = false;
+        String _text = "";
+
+        public Boolean Correct
+        {
+            get
+            {
+                return _correct;
+            }
+            set
+            {
+                _correct = value;
+            }
+        }
+
+        public String DisplayText
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+            }
+        }
+    }
+}
